cfc25948b8bbbe2aae08d703d49377ecb96daadf
diegonobregac <diegonobreas@gmail.com>
2014-02-20 01:12:43 -0300

    CrudProduct

--- src/DAO/DaoProduct.java
+++ src/DAO/DaoProduct.java
@@ -4,10 +4,51 @@
  */
 package DAO;
 
-/**
- *
- * @author Mateus
- */
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.swing.JOptionPane;
+
+import entitys.Product;
+
+
+
 public class DaoProduct {
+	
+	static ArrayList<Product> productList =  new ArrayList<Product>();
+	Iterator<Product> it = productList.iterator();
+	
+	
+	 public boolean saveProduct(Product product){
+
+			it = productList.iterator();
+			/*while(it.hasNext()){
+				if(it.next().getCpf().equalsIgnoreCase(funcionario.getCpf())){
+					//Criar exceção para produto
+					throw new CPFException();
+				}
+			}*/
+			productList.add(product);
+			return true;
+		}
+	 
+	 public void listProduct(){
+			if(productList.isEmpty()){
+				JOptionPane.showMessageDialog(null, "Não existe Produtos cadastrados no sistema");
+				return;
+			}
+			it = productList.iterator();
+			while(it.hasNext()){
+				JOptionPane.showMessageDialog(null, it.next().toString());
+			}
+		}
+	 
+	 public void update(Product product){
+	    	saveProduct(product);
+		}
+	    
+	    public void removeFuncionario(Product product){
+	    	productList.remove(product);
+		}
     
 }
