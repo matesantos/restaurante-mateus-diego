5fc0b690f3ab89dcfaba55a764804fba8010b5e6
diegonobregac <diegonobreas@gmail.com>
2014-02-20 01:12:09 -0300

    CrudClient

--- src/DAO/DaoClient.java
+++ src/DAO/DaoClient.java
@@ -4,20 +4,51 @@
  */
 package DAO;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.swing.JOptionPane;
+
 import entitys.Client;
+import exceptions.IDException;
 
 
-
-/**
- *
- * @author Mateus
- */
 public class DaoClient {
     
+	static ArrayList<Client> clientList =  new ArrayList<Client>();
+	Iterator<Client> it = clientList.iterator();
     
 	
-	public boolean salvarCliente(Client cli){
-		return true;
-	}
+	 public boolean saveClient(Client client) throws IDException{
+
+			it = clientList.iterator();
+			/*while(it.hasNext()){
+				if(it.next().getClientId().equalsIgnoreCase(client.getClientId())){
+					throw new IDException(null);
+				}
+			}*/
+			clientList.add(client);
+			return true;
+		}
+	    
+	    public void listClient(){
+			if(clientList.isEmpty()){
+				JOptionPane.showMessageDialog(null, "Não existe cliente cadastrado no sistema");
+				return;
+			}
+			it = clientList.iterator();
+			while(it.hasNext()){
+				JOptionPane.showMessageDialog(null, it.next().toString());
+			}
+		}
+	    
+	    public void update(Client client) throws IDException{
+	    	saveClient(client);
+		}
+	    
+	    public void removeClient(Client client){
+	    	clientList.remove(client);
+		}
+	    
     
 }
